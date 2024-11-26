d59e5f3d2a5d231a31bc903311826bde45bf75e0
Aaronasa <aaron.soelistiono22@gmail.com>
2024-11-27 01:26:58 +0700

    Launch View

---
+++ app/src/main/java/com/example/foodalp/View/Launch.kt
@@ -0,0 +1,44 @@
+package com.example.foodalp.View
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.foodalp.R
+
+@Composable
+fun Launchview() {
+
+    Column {
+        Box(modifier = Modifier.fillMaxHeight().background(color = Color.White)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.rasalanka),
+                contentDescription = "Logo",
+                modifier = Modifier.fillMaxSize().padding(end = 15.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.group_8),
+                contentDescription = "Background orange",
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview3() {
+    Launchview()
+}
