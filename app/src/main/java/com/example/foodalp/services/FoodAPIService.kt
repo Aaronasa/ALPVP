55de0783b38a74002c26f2ac48368c57e79f8c9b
Shironee29 <153483750+Shironee29@users.noreply.github.com>
2025-01-10 18:00:49 +0700

    Update FoodAPIService.kt

--- app/src/main/java/com/example/foodalp/services/FoodAPIService.kt
+++ app/src/main/java/com/example/foodalp/services/FoodAPIService.kt
@@ -13,30 +13,30 @@ import com.example.foodalp.models.UpdateFoodRequest
 interface FoodAPIService {
 
     @Multipart
-    @POST("api/food/create")
+    @POST("/food/create")
     fun createFood(
-        @Header("Authorization") token: String,
+        @Header("x-API-Token") token: String,
         @Part image: MultipartBody.Part,
         @Part("name") name: RequestBody,
         @Part("description") description: RequestBody,
         @Part("price") price: RequestBody
     ): Call<CreateFoodRequest>
 
-    @GET("api/food/read/{id}")
+    @GET("/food/read/{id}")
     fun getFoodDetail(
-        @Header("Authorization") token: String,
+        @Header("x-API-Token") token: String,
         @Path("id") id: Int
     ): Call<FoodModel>
 
-    @GET("api/food/readall")
+    @GET("/food/readall")
     fun getAllFoods(
-        @Header("Authorization") token: String
+        @Header("x-API-Token") token: String
     ): Call<GetAllFoodsResponse>
 
     @Multipart
-    @PUT("api/food/update")
+    @PUT("/food/update")
     fun updateFood(
-        @Header("Authorization") token: String,
+        @Header("x-API-Token") token: String,
         @Part("id") id: RequestBody,
         @Part image: MultipartBody.Part?,
         @Part("name") name: RequestBody,
